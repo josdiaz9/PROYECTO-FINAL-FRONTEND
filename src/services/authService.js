e65a1dd7f98c14a3ea73c7007b6c6194828766c4
zeke.dev <josdiaz@frba.utn.edu.ar>
2026-06-26 01:21:39 -0300

    configuraciones

---
+++ src/services/authService.js
@@ -0,0 +1,25 @@
+import ENVIRONMENT from '../config/environment'
+export async function login(email, password) {
+
+    const response_http = await fetch(
+        ENVIRONMENT.URL_API + '/api/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify(
+            {
+                email: email,
+                password: password
+            }
+        )
+    }
+    )
+    const response = await response_http.json()
+    if (!response.ok) {
+        throw new Error(response.message)
+    }
+    return response
+
+
+}
